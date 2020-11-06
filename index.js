7b98a0241e85d28afc4f0773d5f0adb6873470d9
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2020-11-05 22:35:11 -0800

    complete task 2

--- index.js
+++ index.js
@@ -53,8 +53,15 @@ export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  /*Your code here*/
+  discount: function(status){
+    if(status === "teacher" || status === "student"){
+      return this.price*0.75;
+    }else if(status === "public"){
+      return this.price*0.90;
+    }
+  }
 }
+console.log(burger.discount("teacher"));
 
 
 
